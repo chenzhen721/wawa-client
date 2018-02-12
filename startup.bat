f78d79c1c5f520adfdce80e0dfd407a13db042e8
zhen.chen <zhen.chen@2339.com>
2018-02-12 15:52:23 +0800

    修改打包方式

--- startup.bat
+++ startup.bat
@@ -1,6 +1,6 @@
 @echo on
 set JAVA_HOME=C:\Program Files (x86)\Java\jdk1.8.0_131
 set classpath=.;%JAVA_HOME%\lib\dt.jar;%JAVA_HOME%\lib\tools.jar;
-set path=C:\Program Files (x86)\Java\jdk1.8.0_131\bin
+set path=%JAVA_HOME%\bin
 java -Djava.library.path="G:/projects/wawa-client/src/main/jniLibs;" -jar target\wawa-client.jar
 pause
