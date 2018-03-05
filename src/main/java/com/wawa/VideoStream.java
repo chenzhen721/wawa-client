1ce0099e435f2ac11dc4aa18f38f41f11d6c7364
zhen.chen <zhen.chen@2339.com>
2018-03-05 18:45:59 +0800

    client

--- src/main/java/com/wawa/VideoStream.java
+++ src/main/java/com/wawa/VideoStream.java
@@ -1,8 +1,10 @@
 package com.wawa;
 
-import java.io.BufferedReader;
+import com.wawa.common.utils.StringUtils;
+
 import java.io.File;
-import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class VideoStream {
 
@@ -11,19 +13,20 @@ public class VideoStream {
 
         try {
             File file = new File("D:/output.mp4");
-            String pullStream = "ffmpeg -f dshow -i video=\"ICT Camera\" -framerate 5 -video_size 320x240 -pix_fmt yuv420p -c:v libx264 -b:v 100k -bufsize 100k -vprofile baseline -tune zerolatency -f rawvideo -";
+            String pullStream = "ffmpeg -f dshow -i video=\"e2eSoft VCam\" -r 1 -framerate 10 -video_size 160x120 -pix_fmt yuv420p -c:v libx264 -b:v 2k -bufsize 1k -vprofile baseline -tune zerolatency -f rawvideo -";
             Process process = Runtime.getRuntime().exec(pullStream);
             while(process.isAlive()) {
                 System.out.println("alive");
                 //System.out.println(process.waitFor());
 
-                StringBuilder sb = new StringBuilder();
+                /*StringBuilder sb = new StringBuilder();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
                 String line;
                 while((line = reader.readLine()) != null) {
 
                 }
-                System.out.println(sb);
+                System.out.println(sb);*/
+                writeByteToHex(process.getInputStream());
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -31,5 +34,16 @@ public class VideoStream {
     }
 
 
+    public static void writeByteToHex(InputStream input) {
+        byte[] tmp = new byte[2048];
+        try {
+            int len;
+            while ((len = input.read(tmp, 0, tmp.length)) > -1) {
+                System.out.println(StringUtils.bytes2HexString(tmp, len));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
